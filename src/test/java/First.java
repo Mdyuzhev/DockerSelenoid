812f5213945b81d7c6fd0e387ab7e23788d3e891
mdyuzhev <mdjugev@gmail.com>
2019-01-25 16:46:13 +0300

    залил код

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -6,7 +6,7 @@ public class First extends TestBase{
 
     @Test
     void testUntitledTestCase() throws InterruptedException {
-
+        //лвдмв
         driver.get("http://sber-ndkn-2.vm.cmx.ru:9081/tax-sunr/sbr-ndkn/login.html");
         Thread.sleep(50000);
     }
